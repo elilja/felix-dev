ac07db1e0480c1addf7d894ff5b2a83615122fb0
Eric Lilja <mindcooler@gmail.com>
2023-06-25 16:27:38 +0200

    Bump maven-invoker-plugin 3.2.1 -> 3.6.0

--- tools/maven-bundle-plugin/src/it/dep-reduced/verify.groovy
+++ tools/maven-bundle-plugin/src/it/dep-reduced/verify.groovy
@@ -21,7 +21,7 @@ File pomFile = new File( basedir, "dependency-reduced-pom.xml" );
 assert pomFile.isFile()
 
 def ns = new groovy.xml.Namespace("http://maven.apache.org/POM/4.0.0")
-def pom = new XmlParser().parse( pomFile )
+def pom = new groovy.xml.XmlParser().parse( pomFile )
 
 assert pom[ns.modelVersion].size() == 1
 assert pom[ns.dependencies][ns.dependency].size() == 1
